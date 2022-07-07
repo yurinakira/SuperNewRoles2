5423e1d85d498652270619fb449152924e21f4e0
satsumaimoamo <meiabao25@gmail.com>
2022-07-07 18:45:24 +0900

    フォルダ移動

--- SNRDevTools/StringsToJson.py
+++ SNRDevTools/StringsToJson.py
@@ -8,7 +8,7 @@ from openpyxl import load_workbook
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 
 IN_FILE = os.path.join(WORKING_DIR, "TransData.xlsx")
-OUT_FILE = os.path.join(WORKING_DIR, "Resources", "translatedata.json")
+OUT_FILE = os.path.join(WORKING_DIR, "SuperNewRoles", "Resources", "translatedata.json")
 
 def stringToJson(in_files):
   stringData = {}
