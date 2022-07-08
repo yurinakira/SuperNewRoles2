96bf04acb596024dd40a989f7fa06342bd800b10
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-07-08 09:48:06 +0900

    Fea-HidePet-harmonypatch-RpcSetPetを用いる事で処理を開始する事が出来た為コミット
    ・[player.RpcSetPet]の部分で、処理が競合しループする為、LateTaskを用いて処理を遅延した。
    
    ・現状RpcSetPetのタイミングが複数回来るとDefaultPetIdが""に上書きされてしまっている為、
    2回目のRawSetPetで自視点からペットが消えてしまう。

--- SuperNewRoles/Patch/HidePetFromOtherVision.cs
+++ SuperNewRoles/Patch/HidePetFromOtherVision.cs
@@ -5,30 +5,53 @@ namespace SuperNewRoles.Patch
     public class HidePetFromOtherVisionPatch
     {
 
+
+        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSetPet))]
         public static class HidePetFromOtherVisionclass
         {
-            public static string DefaultPet;
+            public static string DefaultPetId;
             public static byte DefaultColor;
-
-             [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSetPet))]
-            public static void RPCHidePetFromOtherVision()
+            public static bool Prefix(PlayerControl __instance, [HarmonyArgument(0)] ref string PetId)
             {
                 if (ConfigRoles.HidePetFromOtherVision.Value)
                 {
-                    var Player = PlayerControl.LocalPlayer;
-
-                    DefaultPet = PlayerControl.LocalPlayer.Data.DefaultOutfit.PetId;
-                    DefaultColor = (byte)PlayerControl.LocalPlayer.Data.DefaultOutfit.ColorId;
-
-                    Player.RpcSetPet("");
-                    RPCHidePet.RPCHidePetFromOtherVision(Player, DefaultPet, DefaultColor);
-
-                    SuperNewRolesPlugin.Logger.LogInfo("ペットを表示しません" + DefaultPet + DefaultColor);
+                    SuperNewRolesPlugin.Logger.LogInfo("0-0");
+                    HidePetFromOtherVision(PlayerControl.LocalPlayer);
+                    SuperNewRolesPlugin.Logger.LogInfo("0-1");
                 }
                 else
                 {
+                    SuperNewRolesPlugin.Logger.LogInfo("0-2");
                     SuperNewRolesPlugin.Logger.LogInfo("ペットを表示します");
+                    SuperNewRolesPlugin.Logger.LogInfo("0-3");
                 }
+                return false;
+            }
+            public static void HidePetFromOtherVision(PlayerControl player)
+            {
+
+                SuperNewRolesPlugin.Logger.LogInfo("1-1");
+                DefaultPetId = player.Data.DefaultOutfit.PetId;
+                SuperNewRolesPlugin.Logger.LogInfo("1-2");
+                DefaultColor = (byte)player.Data.DefaultOutfit.ColorId;
+
+                SuperNewRolesPlugin.Logger.LogInfo("1-3");
+
+                /*  LateTaskをかけない場合[player.RpcSetPet]の部分で、処理が競合しループする。
+                    その為、LateTaskを用いて処理を遅延した。*/
+                new LateTask(() =>
+                {
+                    player.RpcSetPet("");
+                    SuperNewRolesPlugin.Logger.LogInfo("1-4");
+                }, 20f, "RpcSetPet");
+
+                new LateTask(() =>
+                {
+                    RPCHidePet.RPCHidePetFromOtherVision(PlayerControl.LocalPlayer, DefaultPetId, DefaultColor);
+                    SuperNewRolesPlugin.Logger.LogInfo("ペットを表示しません:" + "[" + DefaultPetId + "]" + "[" + DefaultColor + "]");
+                }, 25f, "RawSetPet");
+
+                return;
             }
         }
     }
@@ -38,6 +61,8 @@ namespace SuperNewRoles.Patch
         public static void RPCHidePetFromOtherVision(this PlayerControl player, string PetID, int ColorID)
         {
             player.RawSetPet(PetID, ColorID);
+            return;
         }
+
     }
 }
